99f0c3e8ebc640ef36e37124638981c8a74b1db2
Hippo <badrihippo@gmail.com>
2015-09-17 17:42:21 +0530

    Update tests.py to use unittest framework
    And the script's already found its first error! (It was fixed in
    the previous commit).

--- growlinflask/tests.py
+++ growlinflask/tests.py
@@ -1,67 +1,94 @@
-import models
+import unittest
+from models import UserGroup, User, CampusLocation, Item, BorrowPast, BorrowError, AlreadyBorrowed
 import mongoengine as mongo
 from mongoengine.context_managers import switch_db
-test_db_name = 'testdb'
-test_db = mongo.connect(test_db_name, alias='test_db')
 
-def test_borrow_unborrow():
-    with switch_db(models.UserGroup, 'test_db') as UserGroup, switch_db(models.User, 'test_db') as User, switch_db(models.CampusLocation, 'test_db') as CampusLocation,  switch_db(models.Item, 'test_db') as Item, switch_db(models.BorrowPast, 'test_db') as BorrowPast:
+class GrowlinModelTestCase(unittest.TestCase):
+    def setUp(self):
+        test_db = mongo.connect('testdb')
 
-        g = UserGroup(name='Jupiter').save()
-        u1 = User(name='Europa', group=g).save()
-        u2 = User(name='Ganymede', group=g).save()
-        l = CampusLocation(name='Main').save()
-        i1 = Item(title='Nothing', campus_location=l, accession='0').save()
-        i2 = Item(title='Something', campus_location=l, accession='1').save()
+        # Groups
+        self.g1 = UserGroup(name='Mars').save()
+        self.g2 = UserGroup(name='Jupiter').save()
 
-        assert i1.borrow_current is None
-        assert i2.borrow_current is None
+        # Users       
+        self.u1 = User(name='Phobos', group=self.g1).save()
+        self.u2 = User(name='Deimos', group=self.g1).save()
+        self.u3 = User(name='Europa', group=self.g2).save()
+        self.u4 = User(name='Ganymede', group=self.g2).save()
+
+        # Locations
+        self.l1 = CampusLocation(name='Main').save()
+
+        # Items
+        self.i1 = Item(title='Nothing', campus_location=self.l1, accession='1').save()
+        self.i2 = Item(title='Something', campus_location=self.l1, accession='2').save()
+        self.i3 = Item(title='This thing', campus_location=self.l1, accession='3').save()
+        self.i4 = Item(title='That thing', campus_location=self.l1, accession='4').save()
+
+    def tearDown(self):
+        BorrowPast.objects().delete()
+        Item.objects().delete()
+        CampusLocation.objects().delete()
+        User.objects().delete()
+        UserGroup.objects().delete()
+
+class BorrowTestCase(GrowlinModelTestCase):
+
+    def test_borrow_unborrow(self):         
+        assert self.i1.borrow_current is None
+        assert self.i2.borrow_current is None
 
         # Try borrowing some stuff...
         try:
-            u1.borrow(i1)
-        except models.BorrowError: pass
-        assert(i1.borrow_current is None)
+            self.u3.borrow(self.i1)
+        except BorrowError: pass
+        assert(self.i1.borrow_current is None)
 
         # Properly borrow
-        u1.borrow(i1, '0')
-        assert(i1.borrow_current.user == u1)
-        assert(i1.borrow_current.user.name == 'Europa')
+        self.u3.borrow(self.i1, '1')
+        assert(self.i1.borrow_current.user == self.u3)
+        assert(self.i1.borrow_current.user.name == 'Europa')
 
         # Borrow another
-        u1.borrow(i2, '1')
-        assert(i2.borrow_current.user == u1)
-
-        # Check borrowings list
-        assert(u1.get_current_borrowings().count() == 2)
-        assert(u2.get_current_borrowings().count() == 0)
+        self.u3.borrow(self.i2, '2')
+        assert(self.i2.borrow_current.user == self.u3)
 
         # Try to borrow
         try:
-            u2.borrow(i2, '1')
-        except models.AlreadyBorrowed: pass
-        assert (i1.borrow_current.user == u1)
+            self.u2.borrow(self.i2, '2')
+        except AlreadyBorrowed: pass
+        assert (self.i1.borrow_current.user == self.u3)
 
         # Try to unborrow
         try:
-            p = u1.unborrow(i2)
-        except models.BorrowError: pass
-        assert(i2.borrow_current.user == u1)
+            p = self.u3.unborrow(self.i2)
+        except BorrowError: pass
+        assert(self.i2.borrow_current.user == self.u3)
 
         # Actually unborrow
-        p = u1.unborrow(i2, '1')
-        assert(i2.borrow_current is None)
+        p = self.u3.unborrow(self.i2, '2')
+        assert(self.i2.borrow_current is None)
         assert(isinstance(p, BorrowPast))
 
         # Try again (successfully this time) to borrow
-        u2.borrow(i2, '1')
-        assert(i2.borrow_current.user == u2)
+        self.u2.borrow(self.i2, '2')
+        assert(self.i2.borrow_current.user == self.u2)       
 
-        # Cleanup...
+    def test_current_borrow_list(self):
+        # Check borrowings list
+        assert(self.u1.get_current_borrowings().count() == 0)
+        self.u1.borrow(self.i1, '1')
+        self.u1.borrow(self.i2, '2')
+        assert(self.u1.get_current_borrowings().count() == 2)
 
-        UserGroup.objects().delete()
-        User.objects().delete()
-        CampusLocation.objects().delete()
-        Item.objects().delete()
-        BorrowPast.objects().delete()
-        
+    def test_past_borrow_lists(self):
+        assert(self.u1.get_past_borrowings().count() == 0)
+        self.u1.borrow(self.i1, '1')
+        self.u1.borrow(self.i2, '2')
+        self.u1.unborrow(self.i1, '1')
+        assert(self.u1.get_past_borrowings().count() == 1)
+        assert(self.u1.get_past_borrowings()[0].item.title == self.i1.title)
+
+if __name__ == '__main__':
+    unittest.main()
