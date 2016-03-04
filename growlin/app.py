584f35b90dfbd0b03447fd78a2a6038a93a19f9f
Badri Sunderarajan <badrihippo@gmail.com>
2016-03-04 15:30:17 +0530

    Add support for deployment on OpenShift server
    The `wsgi.py` file and auto-detection of environment variables
    means the app can now be deployed as an OpenShift app without any
    additional configuration (except from creating the initial data).

--- growlin/app.py
+++ growlin/app.py
@@ -1,9 +1,28 @@
+import os 
 from flask import Flask
 from flask.ext.admin import Admin
 from flask.ext.principal import Principal
 
 app = Flask(__name__)
-app.config.from_pyfile('../instance/config.py')
+try:
+    app.config.from_pyfile('../instance/config.py')
+except:
+    print 'Unable to load ../instance/config.py'
+    print 'Searching for OpenShift environment variables...'
+    if os.environ.has_key('OPENSHIFT_APP_NAME'):
+        print 'Detected app: %s. Loading configuration variables...'
+        app.config['SECRET_KEY'] = 'abcdef'
+    
+        app.config['MONGODB_DB'] = os.environ.get('OPENSHIFT_APP_NAME')
+        app.config['MONGODB_HOST'] = os.environ.get('OPENSHIFT_MONGODB_DB_HOST')
+        app.config['MONGODB_PORT'] = os.environ.get('OPENSHIFT_MONGODB_DB_PORT')
+        app.config['MONGODB_USERNAME'] = os.environ.get('OPENSHIFT_MONGODB_DB_USERNAME')
+        app.config['MONGODB_PASSWORD'] = os.environ.get('OPENSHIFT_MONGODB_DB_PASSWORD')
+        
+        app.config['GROWLIN_USE_PEEWEE'] = False
+        app.config['DEBUG'] = False
+    else:
+        print 'No OpenShift app detected. Attempt to run with default config.'
 
 # Flag to use peewee driver instead of MongoEngine
 if not app.config.has_key('GROWLIN_USE_PEEWEE'):
