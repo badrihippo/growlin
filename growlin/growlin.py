2e87796054ef8d09c3d2c47388becfc5dbd1bcb6
Hippo <badrihippo@gmail.com>
2016-03-17 13:48:58 +0530

    Add sys.path.append(flask-admin-material) to growlin.py
    So that it's easier to do the import from the python console without
    having to manually add the flask-admin-material submodule path to the
    folder every single time.

--- growlin/growlin.py
+++ growlin/growlin.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+# Add flask-admin-material, which is a submodule of this repo
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),'../flask-admin-material'))
+
+
 from .app import app
 from .auth import *
 from .admin import admin
